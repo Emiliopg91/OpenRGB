25795a42e217973ad29078f2a7d1780948a2b2c4
Myryk <myryk@posteo.de>
2024-11-11 18:07:58 +0100

    removed unnecessary code

--- Controllers/SteelSeriesController/SteelSeriesApexController/RGBController_SteelSeriesApexM500.cpp
+++ Controllers/SteelSeriesController/SteelSeriesApexController/RGBController_SteelSeriesApexM500.cpp
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------*\
 | RGBController_SteelSeriesApexM500.cpp                     |
 |                                                           |
-|   RGBController for SteelSeries Apex 7                    |
+|   RGBController for SteelSeries Apex M500                 |
 |                                                           |
 |   Eric Samuelson (edbgon)                     05 Jul 2020 |
 |                                                           |
@@ -13,21 +13,6 @@
 #include "RGBController_SteelSeriesApexM500.h"
 #include "SteelSeriesApexRegions.h"
 #include "LogManager.h"
-
-static const char* zone_names[] =
-{
-    ZONE_EN_KEYBOARD
-};
-
-static zone_type zone_types[] =
-{
-    ZONE_TYPE_MATRIX
-};
-
-static const unsigned int zone_sizes[] =
-{
-    sizeof(led_names)/sizeof(char*),
-};
 
 /**------------------------------------------------------------------*\
     @name Steel Series APEX
@@ -74,76 +59,11 @@ RGBController_SteelSeriesApexM500::RGBController_SteelSeriesApexM500(SteelSeries
     Breathing.flags      = MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_BRIGHTNESS;
     Breathing.color_mode = MODE_COLORS_NONE;
     modes.push_back(Breathing);
-
-    // SetupZones();
 }
 
 RGBController_SteelSeriesApexM500::~RGBController_SteelSeriesApexM500()
 {
-    /*---------------------------------------------------------*\
-    | Delete the matrix map                                     |
-    \*---------------------------------------------------------*/
-    for(unsigned int zone_index = 0; zone_index < zones.size(); zone_index++)
-    {
-        if(zones[zone_index].matrix_map != NULL)
-        {
-            free(zones[zone_index].matrix_map->map);
-            delete zones[zone_index].matrix_map;
-        }
-    }
-
     delete controller;
-}
-
-void RGBController_SteelSeriesApexM500::SetupZones()
-{
-    LOG_ERROR("steelseries SetupZones\n");
-    /*---------------------------------------------------------*\
-    | Set up zones                                              |
-    \*---------------------------------------------------------*/
-
-    /*---------------------------------------------------------*\
-    | The first 5 chars are the SKU which we need to determine  |
-    | the region.                                               |
-    \*---------------------------------------------------------*/
-
-    std::string sku = serial.substr(0, 5);
-
-    unsigned int total_led_count = 0;
-
-    for(unsigned int zone_idx = 0; zone_idx < 1; zone_idx++)
-    {
-        zone new_zone;
-        new_zone.name           = zone_names[zone_idx];
-        new_zone.type           = zone_types[zone_idx];
-
-        if(zone_types[zone_idx] == ZONE_TYPE_MATRIX)
-        {
-            new_zone.matrix_map         = new matrix_map_type;
-            new_zone.matrix_map->map    = (unsigned int *) malloc(matrix_mapsize*sizeof(unsigned int));
-
-            if((proto_type == APEX) || (proto_type == APEX_M))
-            {
-                SetSkuRegion(*new_zone.matrix_map, sku);
-            }
-        }
-        else
-        {
-            new_zone.matrix_map         = NULL;
-        }
-
-        if((proto_type == APEX) || (proto_type == APEX_M))
-        {
-            new_zone.leds_min   = zone_sizes[zone_idx];
-            new_zone.leds_max   = zone_sizes[zone_idx];
-            new_zone.leds_count = zone_sizes[zone_idx];
-            total_led_count    += zone_sizes[zone_idx];
-        }
-        zones.push_back(new_zone);
-    };
-
-    SetSkuLedNames(leds, sku, total_led_count);
-    SetupColors();
 }
 
 void RGBController_SteelSeriesApexM500::ResizeZone(int /*zone*/, int /*new_size*/)
