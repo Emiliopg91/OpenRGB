3cfefc0d742b862e59b7d4897f1f998adfe1996d
Emiliopg91 <ojosdeserbio@gmail.com>
2025-01-30 11:08:49 +0100

    Scope II 96 logo replacement

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 podman build -t openrgb-build .
 podman run --rm --privileged --cpus=$(nproc) -v $(pwd):/input openrgb-build
-/var/mnt/Datos/Desarrollo/Workspace/VSCode/RogControlCenter/resources/OpenRGB.AppImage --appimage-extract
+/var/mnt/Datos/Desarrollo/Workspace/VSCode/RogControlCenter/assets/OpenRGB.AppImage --appimage-extract
 cp ./build/openrgb squashfs-root/usr/bin/OpenRGB
 /var/mnt/Datos/Desarrollo/Workspace/VSCode/AppImage-Creator-2/resources/appimagetool squashfs-root/ ./OpenRGB-Exp.AppImage
